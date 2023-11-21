9cd111731f76d36af29a727cdde454e896e774cd
DEBABRATA BAR <48706465+debabratabar@users.noreply.github.com>
2023-11-21 10:41:47 +0000

    some minor changes

--- 0-1-js-basics/03-conversion.js
+++ 0-1-js-basics/03-conversion.js
@@ -51,10 +51,10 @@ let str2 = " Debabrata"
 let str3 = str1 + str2
 console.log(str3);
 
-console.log("1" + 2);
-console.log(1 + "2");
-console.log("1" + 2 + 2);
-console.log(1 + 2 + "2");
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
 
 // console.log( (3 + 4) * 5 % 3);
 
@@ -65,10 +65,12 @@ let num1, num2, num3
 
 num1 = num2 = num3 = 2 + 2
 
+console.table([num1 , num2 , num3])//easy way to print multiple  variable
+
+
 let gameCounter = 100
 ++gameCounter;
 // console.log(gameCounter);
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
-

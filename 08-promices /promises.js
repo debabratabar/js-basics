d6cebd7c1c61cab7911f5e3170a4627427f6e0ca
debabratabar <debabrata.bar1999@gmail.com>
2023-12-19 13:03:33 +0530

    practice-Promise

--- 08-promices /promises.js
+++ 08-promices /promises.js
@@ -35,3 +35,42 @@ new Promise((resolve, reject) => {
 .then(() => { console.log("I am in reolve part"); })  // using direct then & catch options 
 .catch((error) => console.log(error))
 
+
+// promise Example 3 
+
+let promiseThree = new Promise( (resolve , reject)=>{
+    setTimeout( () =>{
+        console.log("I am in promiseThree");
+        let state = false
+        if ( state){
+        resolve({user:"debabratabar" , repoCount: 17}) // passing value when Promise resolved  
+        }
+        else{
+            reject('404 ! Not Found') // passing value when Promise rejected 
+        }
+    } ,  2000)
+})
+
+promiseThree.then((user) => {
+    console.log(`UserName : ${user.user} , RepoCount = ${user.repoCount}`);
+}).catch((errData) => {
+    console.log(errData);
+})
+
+
+// Promise Example 4 
+
+
+let promiceArr = [promiseThree , promiceOne]
+ 
+Promise.all(promiceArr) // it takes a arr of promises and returnes a promise and it rejects if any of 
+.then(() => {            // the promises rejects , if all the iterable promises resolved , then it resolved 
+        // console.log(promiceOne.state);
+        // console.log(promiseThree.state);
+})
+.catch( (error) => {
+    console.log("Error");
+    process.on('unhandledRejection' , (reason, promise) =>  {
+        console.error(`Rejected Promise : ${promise}` );
+    })
+})
